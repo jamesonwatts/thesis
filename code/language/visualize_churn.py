6b4e915f53454a1b4b0cc49ddbd2df078515fbe2
Jameson Watts <jameson.watts@gmail.com>
2015-01-16 09:45:12 -0700

    visualize

--- code/language/visualize_churn.py
+++ code/language/visualize_churn.py
@@ -1,28 +1,21 @@
 import pickle
 import numpy as np
 from dateutil import parser
-import os.path
 
 #settings
-ma = 10
-wrange = [50,100,500,1000,2000]   
+ma = 3
 
 dists = []
 xs = []
 #grab data
 for year in range(1991, 2005):
     for month in range(1,13):
-        for day in range(1,31):
-            d = str(year)+'-'+str(month) if month > 9 else str(year)+'-0'+str(month)
-            d = d+'-'+str(day) if day > 9 else d+'-0'+str(day)
-            file_path = '../resources/dy/fdist'+d+'.pkl'
-            if os.path.exists(file_path):         
-                xs.append(parser.parse(d))
-                with open(file_path, 'r') as f:
-                    dists.append(pickle.load(f))
-    print "Done importing year: "+str(year)
+        d = str(year)+'-'+str(month) if month > 9 else str(year)+'-0'+str(month)
+        xs.append(parser.parse(d))
+        with open('../resources/mo/fdist'+d+'.pkl', 'r') as f:
+            dists.append(pickle.load(f))
 
-# create N day distributions
+# create N month distributions
 mdists = []
 for i in range(len(dists)-ma-1):
     fd = dists[i]
@@ -31,13 +24,17 @@ for i in range(len(dists)-ma-1):
     mdists.append(fd)
 print "Done creating MA dists"
 
+
+twords = mdists[0].most_common(50)
+#ewords = mdists[len(mdists)-1].most_common(50)
+
 words = {}
+for k,v in twords:
+    words[k] = []
 for j in range(len(mdists)-1):
-    fd = mdists[j].most_common(10)
-    for i in range(10):
-        words[i] = fd[i]
-    
+    for word in words:
+        words[k].append(mdists[j][word])
    
 #save data to csv
-data = zip([dt.strftime("%Y-%m-%d") for dt in xs[ma:]],words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10])
-np.savetxt("/Users/research/GDrive/Dissertation/thesis/stata/top_words.csv",data,delimiter=",",header="sdate,words,vocab,klent50,klent100,klent500,klent1000,klent2000,churn50,churn100,churn500,churn1000,churn2000,rank50,rank100,rank500,rank1000,rank2000",fmt="%s")
+data = zip([dt.strftime("%Y-%m-%d") for dt in xs[ma:]],words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10],words[11],words[12],words[13],words[14],words[15])
+np.savetxt("/Users/research/GDrive/Dissertation/thesis/stata/top_words.csv",data,delimiter=",",header="sdate,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15",fmt="%s")
